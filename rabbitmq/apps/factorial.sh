da9c600df7a804f5f637d4e4e5bdfe2a68adb311
Wanderson Camargo <wandersonwhcr@gmail.com>
2022-07-18 07:02:59 -0300

    Initialize helper script

---
+++ rabbitmq/apps/factorial.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+set -xe
+
+REQUEST_DATA=`echo "$1" | jq 'tonumber | { number: . }'`
+
+TEMPORARY_FILENAME=`mktemp`
+
+curl http://localhost:3000/factorials \
+    --silent \
+    --request POST \
+    --header 'Content-Type: application/json' \
+    --data "$REQUEST_DATA" > "$TEMPORARY_FILENAME"
+
+FACTORIAL_ID=`jq --raw-output .id "$TEMPORARY_FILENAME"`
+
+sleep 1
+
+curl "http://localhost:3000/factorials/$FACTORIAL_ID" \
+    --include

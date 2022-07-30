88c57508fd6c918bdee68d1f65a6bfb677dfd479
Wanderson Camargo <wandersonwhcr@gmail.com>
2022-07-30 10:34:27 -0300

    Execute while unblocking Event Loop

--- resources/src/app-example/server.js
+++ resources/src/app-example/server.js
@@ -1,1 +1,27 @@
-console.log("Hello, World!");
+console.log(new Date(), 'Application Started');
+
+function handler(signal) {
+  console.info(new Date(), 'Signal Received: ' + signal);
+  console.info(new Date(), 'Application Stopped');
+  process.exit(0);
+}
+
+process.on('SIGINT', handler);
+process.on('SIGTERM', handler);
+
+async function checkEventLoop() {
+  return new Promise(result => setTimeout(result, 0));
+}
+
+async function main() {
+  while (true) {
+    await checkEventLoop();
+  }
+}
+
+main()
+  .catch(error => {
+    console.error(new Date(), error);
+    console.info(new Date(), 'Application Stopped');
+    process.exit(1);
+  });

7e270c430b11296409d601b094c6d0acf92cf8e9
Wanderson Camargo <wandersonwhcr@gmail.com>
2022-08-09 08:00:59 -0300

    Improve output

--- fluent-bit/app-example/assets/entrypoint.sh
+++ fluent-bit/app-example/assets/entrypoint.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env sh
 
-apk add coreutils openssl \
+apk add openssl \
     --quiet
 
 trap exit 1 2 15
 
 while true; do
-    echo `date --rfc-3339 seconds` `openssl rand -hex 20`
+    echo `date +"%Y-%m-%dT%H:%M:%S%z"` `openssl rand -hex 20`
     sleep 1 &
     wait $!
 done

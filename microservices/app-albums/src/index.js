c37015007e23f7911a60525814669375223b3480
Wanderson Camargo <wandersonwhcr@gmail.com>
2022-09-26 22:42:22 -0300

    Create an album

--- microservices/app-albums/src/index.js
+++ microservices/app-albums/src/index.js
@@ -9,11 +9,19 @@ const fastify = require('fastify')({
   },
 });
 
+fastify.register(require('@fastify/mongodb'), {
+  forceClose: true,
+  url: process.env.MONGO_URL,
+  database: process.env.MONGO_DATABASE,
+});
+
 const { name, version, description } = require('./package.json');
 
 fastify.get('/', (request, reply) => {
   reply.status(200)
     .send({ name, version, description });
 });
+
+fastify.post('/v1/albums', require('./routes/create.js'));
 
 module.exports = fastify;
---
+++ microservices/app-albums/src/routes/create.js
@@ -0,0 +1,56 @@
+const uuid = require('uuid');
+const { Binary } = require('mongodb');
+
+module.exports = {
+  schema: {
+    body: {
+      type: 'object',
+      required: ['name', 'artists'],
+      additionalProperties: false,
+      properties: {
+        name: { type: 'string' },
+        artists: {
+          type: 'array',
+          minItems: 1,
+          items: {
+            type: 'object',
+            required: ['_id'],
+            additionalProperties: false,
+            properties: {
+              _id: { type: 'string', format: 'uuid' },
+            },
+          },
+        },
+      },
+    },
+    response: {
+      201: {
+        type: 'object',
+        required: ['_id'],
+        additionalProperties: false,
+        properties: {
+          _id: { type: 'string', format: 'uuid' },
+        },
+      },
+    },
+  },
+
+  handler: async function (request, reply) {
+    const _id = uuid.v4();
+
+    request.body.artists.forEach((artist) => {
+      artist._id = new Binary(uuid.parse(artist._id), Binary.SUBTYPE_UUID);
+    });
+
+    await this.mongo.db.collection('albums')
+      .insertOne({
+        _id: new Binary(uuid.parse(_id), Binary.SUBTYPE_UUID),
+        ...request.body,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      });
+
+    reply.status(201)
+      .send({ _id });
+  },
+};

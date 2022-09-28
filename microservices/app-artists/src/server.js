51313bfbc17a2f1b9e7c686a0a73e65b1999ec36
Wanderson Camargo <wandersonwhcr@gmail.com>
2022-09-28 08:03:35 -0300

    Separate Tracing

--- microservices/app-artists/src/server.js
+++ microservices/app-artists/src/server.js
@@ -1,18 +1,3 @@
-const opentelemetry = require('@opentelemetry/sdk-node');
-const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-http');
-const { HttpInstrumentation } = require('@opentelemetry/instrumentation-http');
-
-const sdk = new opentelemetry.NodeSDK({
-  traceExporter: new OTLPTraceExporter(),
-  instrumentations: [
-    new HttpInstrumentation(),
-  ],
-});
-
-sdk.start();
-
-// -------------------------------------------------------------------------------------------------
-
 const fastify = require('.');
 
 fastify.listen({ host: '0.0.0.0', port: process.env.PORT })

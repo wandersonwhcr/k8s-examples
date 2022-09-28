479e41750fb197cc98778ef9c64a13e9fbf561b1
Wanderson Camargo <wandersonwhcr@gmail.com>
2022-09-28 08:07:14 -0300

    Install MongoDB Instrumentation

--- microservices/app-artists/src/tracing.js
+++ microservices/app-artists/src/tracing.js
@@ -1,12 +1,14 @@
 const opentelemetry = require('@opentelemetry/sdk-node');
 const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-http');
 const { HttpInstrumentation } = require('@opentelemetry/instrumentation-http');
+const { MongoDBInstrumentation } = require('@opentelemetry/instrumentation-mongodb');
 
 const sdk = new opentelemetry.NodeSDK({
   // traceExporter: new opentelemetry.tracing.ConsoleSpanExporter(),
   traceExporter: new OTLPTraceExporter(),
   instrumentations: [
     new HttpInstrumentation(),
+    new MongoDBInstrumentation(),
   ],
 });
 
